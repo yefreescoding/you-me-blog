b5fad2f58ce08b2429295897404419700a3d145d
Yefree Valdez <yefree13santos@outlook.com>
2023-06-02 12:45:27 +0200

    loader added

---
+++ you-me-blog/src/js/eventListners.js
@@ -0,0 +1,19 @@
+const navLinks = document.querySelectorAll('[data-navLink]');
+
+navLinks.forEach((link) => {
+  if (link.getAttribute('href') === window.location.pathname) {
+    link.setAttribute('aria-current', 'page');
+  }
+});
+
+const preloader = document.querySelector('.preloader');
+
+// window.addEventListener('load', () => {
+//   preloader.classList.add('loaded');
+// });
+
+const loaderTime = setTimeout(loading, 1500);
+
+function loading() {
+  preloader.classList.add('loaded');
+}
